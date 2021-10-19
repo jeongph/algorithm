cbf9ba78cb62e87306f2d529a0f563616bc38a1f
Jeong-Uk Park <dnr1105@gmail.com>
2021-10-19 21:44:58 +0900

    Delete programmers directory

--- programmers/all_challenges/12969/Solution.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    val (a, b) = readLine()!!.split(' ').map(String::toInt)
-
-    for (line in 0..b) {
-        for (star in 0..a)
-            print("*")
-        println()
-    }
-}
